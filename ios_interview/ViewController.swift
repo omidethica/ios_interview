ff5a38813241607888c8c4af56341497fa316227
Amin Tavassolian <amin.tavassolian@gmail.com>
2021-02-22 09:24:56 -0500

    Fixed cycles in the ViewController

--- ios_interview/ViewController.swift
+++ ios_interview/ViewController.swift
@@ -9,11 +9,81 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    static let DATA_COLLECTION_PAUSE_IN_S: Double = 3600
+    static let TIME_PER_SCAN: TimeInterval = 60
+    static let TIME_BETWEEN_SCANS: TimeInterval = 300
+    static var periodicScanningTimer: Timer!
+    static var performScanningTimer: Timer!
+    private var readingActive = false
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        refreshStreams()
     }
 
+    func refreshStreams() {
+        self.stopAllScans()
+
+        let result = LocationManager.getInstance().startUpdatingLocation(collectionType: .ALWAYS)
+        guard result else {
+            return
+        }
+
+        self.initializePeriodicScanningTimer()
+    }
+
+    func stopAllScans() {
+        self.readingActive = false
+
+        if ViewController.performScanningTimer != nil {
+            ViewController.performScanningTimer.invalidate()
+        }
+
+        if ViewController.periodicScanningTimer != nil {
+            ViewController.periodicScanningTimer.invalidate()
+        }
+
+        // save all the recorded data.
+
+        LocationManager.getInstance().stopUpdatingLocation()
+    }
+
+    fileprivate func initializePeriodicScanningTimer() {
+
+        ViewController.periodicScanningTimer =
+            Timer.scheduledTimer(timeInterval: ViewController.TIME_BETWEEN_SCANS,
+                                 target: self,
+                                 selector: #selector(self.initializePerformScanningTimer),
+                                 userInfo: nil,
+                                 repeats: true)
+        ViewController.periodicScanningTimer.fire()
+    }
+
+    @objc fileprivate func initializePerformScanningTimer() {
+        ViewController.performScanningTimer =
+            Timer.scheduledTimer(timeInterval: ViewController.TIME_PER_SCAN,
+                                 target: self,
+                                 selector: #selector(self.scan),
+                                 userInfo: nil,
+                                 repeats: true)
+        ViewController.performScanningTimer.fire()
+    }
+
+    @objc fileprivate func scan() {
+
+        if !self.readingActive {
+            LocationManager.getInstance().changeLocationAccuracy(newAccuracy: .BEST)
+        } else {
+            self.readingActive = false
+
+            LocationManager.getInstance().changeLocationAccuracy(newAccuracy: .THREE_KILOMETERS)
+            
+            if ViewController.performScanningTimer != nil {
+                ViewController.performScanningTimer.invalidate()
+                ViewController.performScanningTimer = nil
+            }
+        }
+    }
 
 }
 
